5a626d21d8a3a1475ee75382fa52e6828d72c65f
KenDual <maiphuhai123@gmail.com>
2025-10-15 00:44:44 +0700

    Initial clean commit

---
+++ src/main/java/ecom/intensifier/credibilityengine/CredibilityengineApplication.java
@@ -0,0 +1,13 @@
+package ecom.intensifier.credibilityengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CredibilityengineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CredibilityengineApplication.class, args);
+	}
+
+}
